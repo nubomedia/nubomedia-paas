38ccd52f479978a885f36645e31fc02d6f481b74
maa <maa@fokus.fraunhofer.de>
2015-10-22 17:54:19 +0200

    using json for deserializing

--- src/main/java/org/project/openbaton/nubomedia/api/NubomediaAppManager.java
+++ src/main/java/org/project/openbaton/nubomedia/api/NubomediaAppManager.java
@@ -201,10 +201,11 @@ public class NubomediaAppManager {
 
     @RequestMapping(value = "/openbaton/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public void startOpenshiftBuild(@RequestBody OpenbatonEvent evt, @PathVariable("id") String id){
+    public void startOpenshiftBuild(@RequestBody String evt, @PathVariable("id") String id){
         logger.debug("starting callback for appId" + id);
         logger.info("Received event " + evt);
         Application app = appRepo.findOne(id);
+        OpenbatonEvent myevt = new GsonBuilder().create().fromJson(evt,OpenbatonEvent.class);
 //        if(evt.getAction().equals(Action.INSTANTIATE_FINISH)){
             OpenbatonCreateServer server = deploymentMap.get(id);
             logger.debug("retrieved session for " + server.getToken());
