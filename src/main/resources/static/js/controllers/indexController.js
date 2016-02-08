faf992409ed84d27bdfa5b5ecbc83c44c1425add
nicobove <domenico.bove@fokus.fraunhofer.de>
2016-02-08 20:48:18 +0100

    Added App log button

--- src/main/resources/static/js/controllers/indexController.js
+++ src/main/resources/static/js/controllers/indexController.js
@@ -10,8 +10,8 @@ var app = angular.module('app');
 app.controller('LoginController', function ($scope, AuthService, Session, $rootScope, $location, $cookieStore, $http) {
     $scope.currentUser = null;
     //$scope.URL = 'http://localhost:8080';
-    //$scope.URL= 'http://80.96.122.84:8081';
-    $scope.URL = '';
+    $scope.URL= 'http://80.96.122.84:8081';
+    //$scope.URL = '';
     $scope.credential = {
         "username": '',
         "password": ''
