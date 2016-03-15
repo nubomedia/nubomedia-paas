ad25374d70044c3fb30e59c6bb70c5cee96c6c75
Carlo <charliemaiors@gmail.com>
2016-03-15 19:08:40 +0100

    increasing logs

--- src/main/java/org/project/openbaton/nubomedia/api/openshift/beans/DeploymentManager.java
+++ src/main/java/org/project/openbaton/nubomedia/api/openshift/beans/DeploymentManager.java
@@ -162,7 +162,7 @@ public class DeploymentManager {
         for(String pod : podList.getPodNames()){
             logger.debug("CURRENT POD IS " + pod);
             logger.debug("APP NAME IS " + appName);
-            logger.debug("Pod satisfy the requirements? " + pod.contains(appName + "-dc-1)"));
+            logger.debug("Pod satisfy the requirements? " + pod.contains(appName + "-dc-)"));
             if (pod.contains(appName + "-dc-1)")) {
                 logger.debug("Running with pod " + pod);
                 if (!pod.contains("bc-1-build") || !pod.contains("-deploy")) {
