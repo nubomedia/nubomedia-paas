0535f57a21eaaae5ae60a7508545b34760398bd3
nicobove <nicobb22@gmail.com>
2016-04-13 12:30:13 +0200

    Updated the Application info page

--- src/main/resources/static/js/controllers/indexController.js
+++ src/main/resources/static/js/controllers/indexController.js
@@ -9,7 +9,7 @@ var app = angular.module('app');
 
 app.controller('LoginController', function ($scope, AuthService, Session, $rootScope, $location, $cookieStore, $http) {
     $scope.currentUser = null;
-    //$scope.URL = 'http://localhost:8080';
+    //$scope.URL = 'http://localhost:8081';
     //$scope.URL= 'http://80.96.122.73:8081';
     $scope.URL = '';
     $scope.credential = {
