ef51889d051f7dc99232db34f37a585c8dbfe554
Michael Pauls <michael.pauls@tu-berlin.de>
2016-10-24 11:59:35 +0200

    Add functions for scaling and start/stop

--- src/main/java/org/project/openbaton/nubomedia/paas/core/OpenbatonManager.java
+++ src/main/java/org/project/openbaton/nubomedia/paas/core/OpenbatonManager.java
@@ -19,11 +19,12 @@
 
 package org.project.openbaton.nubomedia.paas.core;
 
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.openbaton.catalogue.mano.common.ConnectionPoint;
 import org.openbaton.catalogue.mano.common.LifecycleEvent;
-import org.openbaton.catalogue.mano.descriptor.NetworkServiceDescriptor;
-import org.openbaton.catalogue.mano.descriptor.VirtualDeploymentUnit;
-import org.openbaton.catalogue.mano.descriptor.VirtualNetworkFunctionDescriptor;
+import org.openbaton.catalogue.mano.descriptor.*;
 import org.openbaton.catalogue.mano.record.NetworkServiceRecord;
+import org.openbaton.catalogue.mano.record.Status;
 import org.openbaton.catalogue.mano.record.VNFCInstance;
 import org.openbaton.catalogue.mano.record.VirtualNetworkFunctionRecord;
 import org.openbaton.catalogue.nfvo.Configuration;
@@ -33,6 +34,7 @@ import org.openbaton.sdk.api.exception.SDKException;
 import org.project.openbaton.nubomedia.paas.core.util.NSDUtil;
 import org.project.openbaton.nubomedia.paas.core.util.NSRUtil;
 import org.project.openbaton.nubomedia.paas.core.util.VIMUtil;
+import org.project.openbaton.nubomedia.paas.exceptions.BadRequestException;
 import org.project.openbaton.nubomedia.paas.exceptions.NotFoundException;
 import org.project.openbaton.nubomedia.paas.exceptions.StateException;
 import org.project.openbaton.nubomedia.paas.exceptions.openbaton.StunServerException;
@@ -40,6 +42,7 @@ import org.project.openbaton.nubomedia.paas.exceptions.openbaton.turnServerExcep
 import org.project.openbaton.nubomedia.paas.messages.AppStatus;
 import org.project.openbaton.nubomedia.paas.model.persistence.Application;
 import org.project.openbaton.nubomedia.paas.model.persistence.openbaton.Flavor;
+import org.project.openbaton.nubomedia.paas.model.persistence.openbaton.Host;
 import org.project.openbaton.nubomedia.paas.model.persistence.openbaton.MediaServerGroup;
 import org.project.openbaton.nubomedia.paas.model.persistence.openbaton.QoS;
 import org.project.openbaton.nubomedia.paas.properties.KmsProperties;
@@ -104,6 +107,7 @@ public class OpenbatonManager {
   public MediaServerGroup createMediaServerGroup(
       String appName,
       String osName,
+      int numberOfInstances,
       Flavor flavorID,
       String callbackUrl,
       boolean cloudRepositorySet,
@@ -140,6 +144,7 @@ public class OpenbatonManager {
     // building network service descriptor
     NetworkServiceDescriptor targetNSD =
         NSDUtil.getNSD(
+            numberOfInstances,
             flavorID,
             qos,
             turnServerActivate,
@@ -204,7 +209,7 @@ public class OpenbatonManager {
     return mediaServerGroup;
   }
 
-  public void updateMediaServerGroup(Application application) throws Exception {
+  public MediaServerGroup updateMediaServerGroup(Application application) throws Exception {
     NetworkServiceRecord nsr = null;
     try {
       nsr =
@@ -216,14 +221,34 @@ public class OpenbatonManager {
       logger.debug(message);
       throw new Exception(message);
     }
+    application.getMediaServerGroup().setStatus(nsr.getStatus());
     for (VirtualNetworkFunctionRecord record : nsr.getVnfr()) {
-
       if (record.getEndpoint().equals("media-server")) {
         logger.debug("found record media-server");
-        application.getMediaServerGroup().setFloatingIPs(NSRUtil.getFloatingIPs(record));
-        application.getMediaServerGroup().setHostnames(NSRUtil.getHostnames(record));
+        Set<Host> updatedHosts = new HashSet<>();
+        Set<Host> receivedHosts = NSRUtil.getHosts(record);
+        Set<Host> oldHosts = application.getMediaServerGroup().getHosts();
+        for (Host receivedHost : receivedHosts) {
+          boolean foundHost = false;
+          for (Host oldHost : oldHosts) {
+            if (receivedHost.getHostname().equals(oldHost.getHostname())) {
+              oldHost.setStatus(receivedHost.getStatus());
+              oldHost.setFloatingIp(receivedHost.getFloatingIp());
+              updatedHosts.add(oldHost);
+              foundHost = true;
+              break;
+            }
+          }
+          if (!foundHost) {
+            updatedHosts.add(receivedHost);
+          }
+        }
+        application.getMediaServerGroup().setHosts(updatedHosts);
+        //        application.getMediaServerGroup().setFloatingIPs(NSRUtil.getFloatingIPs(record));
+        //        application.getMediaServerGroup().setHostnames(NSRUtil.getHostnames(record));
       }
     }
+    return application.getMediaServerGroup();
   }
 
   public AppStatus getStatus(String nsrID) {
@@ -290,7 +315,7 @@ public class OpenbatonManager {
   }
 
   public void startVnfcInstance(Application app, String hostname)
-      throws SDKException, NotFoundException, StateException {
+      throws SDKException, NotFoundException, BadRequestException {
     NetworkServiceRecord nsr = null;
     try {
       nsr =
@@ -313,7 +338,7 @@ public class OpenbatonManager {
                     .startVNFCInstance(
                         nsr.getId(), vnfr.getId(), msVdu.getId(), mediaServer.getId());
               } else {
-                throw new StateException(
+                throw new BadRequestException(
                     "Bad request: media server "
                         + hostname
                         + " is in wrong state. Must be INACTIVE to start KMS again");
@@ -329,7 +354,7 @@ public class OpenbatonManager {
   }
 
   public void stopVnfcInstance(Application app, String hostname)
-      throws SDKException, NotFoundException, StateException {
+      throws SDKException, NotFoundException, BadRequestException {
     NetworkServiceRecord nsr = null;
     try {
       nsr =
@@ -352,7 +377,7 @@ public class OpenbatonManager {
                     .stopVNFCInstance(
                         nsr.getId(), vnfr.getId(), msVdu.getId(), mediaServer.getId());
               } else {
-                throw new StateException(
+                throw new BadRequestException(
                     "Bad request: media server "
                         + hostname
                         + " is in wrong state. Must be ACTIVE to stop the KMS");
@@ -365,5 +390,125 @@ public class OpenbatonManager {
     if (foundKMS == false) {
       throw new NotFoundException("Not found media server " + hostname);
     }
+  }
+
+  public void scaleInVnfcInstance(Application app, String hostname)
+      throws SDKException, ClassNotFoundException, NotFoundException, BadRequestException {
+    NetworkServiceRecord nsr =
+        nfvoRequestor.getNetworkServiceRecordAgent().findById(app.getMediaServerGroup().getNsrID());
+    if (nsr == null) {
+      throw new NotFoundException("Not found NSR " + app.getMediaServerGroup().getNsrID());
+    }
+    checkScalability(nsr);
+    VirtualNetworkFunctionRecord msVnfr = null;
+    for (VirtualNetworkFunctionRecord vnfr : nsr.getVnfr()) {
+      if (vnfr.getType().equals("media-server")) {
+        msVnfr = vnfr;
+      }
+    }
+    if (msVnfr == null) {
+      throw new NotFoundException("Not found VNFR of type media-server");
+    }
+    VirtualDeploymentUnit msVdu = null;
+    VNFCInstance vnfcInstanceToRemove = null;
+    boolean scalingAllowed = false;
+    for (VirtualDeploymentUnit vdu : msVnfr.getVdu()) {
+      if (vdu.getVnfc_instance().size() > 1) {
+        scalingAllowed = true;
+        for (VNFCInstance vnfcInstance : vdu.getVnfc_instance()) {
+          if (vnfcInstance.getHostname().equals(hostname)) {
+            msVdu = vdu;
+            vnfcInstanceToRemove = vnfcInstance;
+          }
+        }
+      } else {
+        logger.warn("Number of Media Servers reached already minimum of " + app.getName());
+      }
+    }
+    if (scalingAllowed == false) {
+      throw new BadRequestException(
+          "Scaling in not permitted. Minimum number (1) of Media Servers of "
+              + app.getName()
+              + " reached");
+    }
+    if (vnfcInstanceToRemove == null) {
+      throw new NotFoundException("Not found media server " + hostname + " to scale-in");
+    }
+    nfvoRequestor
+        .getNetworkServiceRecordAgent()
+        .deleteVNFCInstance(
+            nsr.getId(), msVnfr.getId(), msVdu.getId(), vnfcInstanceToRemove.getId());
+  }
+
+  private void checkScalability(NetworkServiceRecord nsr) throws BadRequestException {
+    if (nsr.getStatus().ordinal() == Status.ACTIVE.ordinal()) {
+      logger.debug("Scaling allowed since NSR is in ACTIVE state");
+    } else {
+      logger.debug(
+          "Scaling not allowed for since NSR is in "
+              + nsr.getStatus()
+              + " state. Have to be in ACTIVE state");
+      throw new BadRequestException(
+          "Scaling not allowed since Application is in "
+              + nsr.getStatus()
+              + " state. Have to be in ACTIVE state");
+    }
+  }
+
+  public void scaleOutVnfcInstance(Application app)
+      throws SDKException, NotFoundException, ClassNotFoundException, BadRequestException {
+    NetworkServiceRecord nsr =
+        nfvoRequestor.getNetworkServiceRecordAgent().findById(app.getMediaServerGroup().getNsrID());
+    if (nsr == null) {
+      throw new NotFoundException("Not found NSR " + app.getMediaServerGroup().getNsrID());
+    }
+    checkScalability(nsr);
+    VirtualNetworkFunctionRecord msVnfr = null;
+    for (VirtualNetworkFunctionRecord vnfr : nsr.getVnfr()) {
+      if (vnfr.getType().equals("media-server")) {
+        msVnfr = vnfr;
+      }
+    }
+    if (msVnfr == null) {
+      throw new NotFoundException("Not found VNFR of type media-server");
+    }
+    VirtualDeploymentUnit msVdu = null;
+    VNFComponent vnfComponentToAdd = null;
+    boolean scalingAllowed = false;
+    int maximumNumber = 0;
+    for (VirtualDeploymentUnit vdu : msVnfr.getVdu()) {
+      maximumNumber = vdu.getScale_in_out();
+      if (vdu.getScale_in_out() > vdu.getVnfc_instance().size()) {
+        scalingAllowed = true;
+        for (VNFComponent vnfComponent : vdu.getVnfc()) {
+          msVdu = vdu;
+          vnfComponentToAdd = new VNFComponent();
+          Set<VNFDConnectionPoint> connectionPoints = new HashSet<>();
+          for (VNFDConnectionPoint cpCopy : vnfComponent.getConnection_point()) {
+            VNFDConnectionPoint newCP = new VNFDConnectionPoint();
+            newCP.setVirtual_link_reference(cpCopy.getVirtual_link_reference());
+            newCP.setFloatingIp(cpCopy.getFloatingIp());
+            connectionPoints.add(newCP);
+          }
+          vnfComponentToAdd.setConnection_point(connectionPoints);
+        }
+      } else {
+        logger.warn("Number of Media Servers reached already maximum of " + app.getName());
+      }
+    }
+    if (scalingAllowed == false) {
+      throw new BadRequestException(
+          "Scaling out not permitted. Maxmimum number ("
+              + maximumNumber
+              + ") of Media Servers of "
+              + app.getName()
+              + " reached");
+    }
+    if (vnfComponentToAdd == null) {
+      throw new NotFoundException("Not found existing VNFComponent to create a new one.");
+    }
+    nfvoRequestor
+        .getNetworkServiceRecordAgent()
+        .createVNFCInstance(nsr.getId(), msVnfr.getId(), msVdu.getId(), vnfComponentToAdd);
   }
 }
